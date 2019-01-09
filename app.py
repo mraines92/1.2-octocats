28496a7037507d708f9045fb085197529f7fe6c3
Student <student@students-air.attlocal.net>
2019-01-09 07:31:19 -0500

    ran out of time

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 
 
-click_count =
+import os
+
+from flask import Flask         #i copied this part over from the in-class example, because i figured it needed to pull the same
+                                #code over from Flask.
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+#this counter is killing me! HEEEELLLLPPP!!!
+#I really have tried looking at resources, but I can't understand any of it!!
+
+#I dont think this makes any sense, but I was trying to start SOMEWHERE.
+
+visit_count = 0
+
+if visit_count < 1
+
+    visit_count = visit_count + 1
+
+    print(visit_count) #I know this will only work once, and NOT when page is visited. I ran out of time :(
